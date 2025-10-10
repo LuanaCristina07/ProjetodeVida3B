945f421361e58880928fc43802bcf16745cd1ff7
LuanaCristina07 <164919276+LuanaCristina07@users.noreply.github.com>
2025-10-10 14:25:37 -0300

    ...

--- main.js
+++ main.js
@@ -1,2 +1,12 @@
 document.querySelectorAll(".botao")
 document.querySelectorAll(".aba-conteudo")
+for (let i=0; <botoes.length;i++){
+    botoes[i].onclick = function (){
+        for (letj=0;j <botoes.length;j++){
+            botoes[j].clasList.remove("ativo")
+            textos[j].clasList.remove("ativo")
+        }
+        botoes[i].clasList.add("ativo")
+        textos[i].clasList.add("ativo")
+    }
+}
